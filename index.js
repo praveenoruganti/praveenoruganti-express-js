cc7cbc3dc83bf392b3f8440652e39704b9af3b03
Praveen Oruganti <praveenorugantitech@gmail.com>
2020-10-16 22:26:30 +0530

    MySQL

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const mysql = require('mysql');
 app.use(express.json());
 
 app.use(express.urlencoded({ extended: false }));
@@ -23,6 +24,24 @@ app.get("/:word/echo", (req, res) => {
 app.post("/name", function (req, res) {
   var string = req.body.first + " " + req.body.last;
   res.json({ name: string });
+});
+
+require("./mySQL/query")(app);
+
+const sqlParams = {
+  connectionLimit: 1,
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "password",
+  database: "praveendb",
+};
+
+let connection = mysql.createPool(sqlParams);
+module.exports.connection = connection;
+
+app.get("/info", (req, res) => {
+  res.send(" App is up and running....");
 });
 
 app.use(express.static("public"));
