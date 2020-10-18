03c1ee9dcc3efec411b4149f68b4858496e9a261
Praveen Oruganti <praveenorugantitech@gmail.com>
2020-10-18 15:46:44 +0530

    Express JS

--- routes/cookies/cookie.js
+++ routes/cookies/cookie.js
@@ -1,28 +1,11 @@
 module.exports = (app) => {
-//   app.get("/setCookie", function (req, res) {
-//     res.cookie("cookie_name", "cookie_value");
-//     res.cookie("company", "PraveenOruganti Technologies");
-//     res.cookie("name", "Praveen Oruganti");
-
-//     res.status(200).send("Cookie is set");
-//   });
+  app.get("/setCookie", function (req, res) {
+    res.cookie("cookie_name", "cookie_value");
+    res.cookie("company", "PraveenOruganti Technologies");
+    res.cookie("name", "Praveen Oruganti");
+    res.status(200).send("Cookie is set");
+  });
   app.get("/getCookie", function (req, res) {
     res.status(200).send(req.cookies);
-  }); 
-
-  app.use(function (req, res, next) {
-    // check if client sent cookie
-    var cookie = req.cookies.cookieName;
-    if (cookie === undefined) {
-      // no: set a new cookie
-      var randomNumber=Math.random().toString();
-      randomNumber=randomNumber.substring(2,randomNumber.length);
-      res.cookie('cookieName',randomNumber, { maxAge: 900000, httpOnly: true });
-      console.log('cookie created successfully');
-    } else {
-      // yes, cookie was already present 
-      console.log('cookie exists', cookie);
-    } 
-    next(); // <-- important!
   });
 };
